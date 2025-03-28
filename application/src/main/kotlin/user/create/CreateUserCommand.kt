ad5d8274bb4b54073cfc2e585a580621ac09184c
Thales Nishida <nishida.tn@proton.me>
2025-03-28 08:57:53 -0300

    application: creating a unit test to validating createUserTest

--- application/src/main/kotlin/user/create/CreateUserCommand.kt
+++ application/src/main/kotlin/user/create/CreateUserCommand.kt
@@ -1,18 +1,18 @@
 package br.com.thalesnishida.syncbar.application.user.create
 
 data class CreateUserCommand(
-    val name: String,
-    val email: String,
-    val password: String,
-    val typeUser: String,
+    val name: String?,
+    val email: String?,
+    val password: String?,
+    val typeUser: String?,
     val deactivate: Boolean?
 ) {
     companion object {
         fun with(
-            aName: String,
-            aEmail: String,
-            aPassword: String,
-            aTypeUser: String,
+            aName: String?,
+            aEmail: String?,
+            aPassword: String?,
+            aTypeUser: String?,
             aDeactivate: Boolean?
         ) : CreateUserCommand {
             return CreateUserCommand(aName, aEmail, aPassword, aTypeUser, aDeactivate)
--- application/src/test/kotlin/br/com/thalesnishida/syncbar/application/user/create/CreateUserUseCaseTest.kt
+++ application/src/test/kotlin/br/com/thalesnishida/syncbar/application/user/create/CreateUserUseCaseTest.kt
@@ -1,13 +1,29 @@
 package br.com.thalesnishida.syncbar.application.user.create
 
+import exceptions.DomainException
+import io.mockk.MockKAnnotations
 import io.mockk.every
-import io.mockk.mockk
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
 import io.mockk.verify
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import user.UserGateway
 
 class CreateUserUseCaseTest {
+
+    @MockK
+    private lateinit var userGateway: UserGateway
+
+    @InjectMockKs
+    private lateinit var useCase: DefaultCreateUserUseCase
+
+    @BeforeEach
+    fun setUp() {
+        MockKAnnotations.init(this)
+    }
 
     @Test
     fun givenAValidParams_whenCallCreateUser_thenReturnSuccess() {
@@ -19,11 +35,7 @@ class CreateUserUseCaseTest {
 
         val aCommand = CreateUserCommand.with(expectName, expectEmail, expectPassword, expectTypeUser, expectActivate)
 
-        val userGateway: UserGateway = mockk()
-
         every { userGateway.create(any()) } answers { firstArg() }
-
-        val useCase = DefaultCreateUserUseCase(userGateway)
 
         val actualOutput = useCase.execute(aCommand)
 
@@ -39,7 +51,110 @@ class CreateUserUseCaseTest {
                         expectActivate == aUser.isActivate
             })
         }
+    }
 
+    @Test
+    fun givenAInvalidParamsNameLess3Character_whenCallsCreateUser_thenReturnDomainException() {
+        val expectName = "2"
+        val expectEmail = "test@test.com"
+        val expectPassword = "testdaaq12@"
+        val expectTypeUser = "ADMIN"
+        val expectActivate = true
+        val exceptErrorMessage = "'name' must between 3 characters and 255 characters"
+
+        val aCommand = CreateUserCommand.with(expectName, expectEmail, expectPassword, expectTypeUser, expectActivate)
+
+        val actualException = Assertions.assertThrows(DomainException::class.java) { useCase.execute(aCommand) }
+        Assertions.assertEquals(exceptErrorMessage, actualException.getErrors()[0].message)
+
+        verify(exactly = 0) { userGateway.create(any()) }
+    }
+
+    @Test
+    fun givenAInvalidParamsNullName_whenCallsCreateUser_thenReturnDomainException() {
+        val expectName = null
+        val expectEmail = "test@test.com"
+        val expectPassword = "testdaaq12@"
+        val expectTypeUser = "ADMIN"
+        val expectActivate = true
+        val exceptErrorMessage = "'name' should not be null"
+
+        val aCommand = CreateUserCommand.with(expectName, expectEmail, expectPassword, expectTypeUser, expectActivate)
+
+        val actualException = Assertions.assertThrows(DomainException::class.java) { useCase.execute(aCommand) }
+        Assertions.assertEquals(exceptErrorMessage, actualException.getErrors()[0].message)
+
+        verify(exactly = 0) { userGateway.create(any()) }
+    }
+
+    @Test
+    fun givenAInvalidParamsNullEmail_whenCallsCreateUser_thenReturnDomainException() {
+        val expectName = "Test Test"
+        val expectEmail = null
+        val expectPassword = "testdaaq12@"
+        val expectTypeUser = "ADMIN"
+        val expectActivate = true
+        val exceptErrorMessage = "'email' should not be null"
+
+        val aCommand = CreateUserCommand.with(expectName, expectEmail, expectPassword, expectTypeUser, expectActivate)
+
+        val actualException = Assertions.assertThrows(DomainException::class.java) { useCase.execute(aCommand) }
+        Assertions.assertEquals(exceptErrorMessage, actualException.getErrors()[0].message)
+
+        verify(exactly = 0) { userGateway.create(any()) }
+    }
+
+    @Test
+    fun givenAInvalidParamsNullPassword_whenCallsCreateUser_thenReturnDomainException() {
+        val expectName = "Test Test"
+        val expectEmail = "teste@test.com"
+        val expectPassword = null
+        val expectTypeUser = "ADMIN"
+        val expectActivate = true
+        val exceptErrorMessage = "'password' should not be null"
+
+        val aCommand = CreateUserCommand.with(expectName, expectEmail, expectPassword, expectTypeUser, expectActivate)
+
+        val actualException = Assertions.assertThrows(DomainException::class.java) { useCase.execute(aCommand) }
+        Assertions.assertEquals(exceptErrorMessage, actualException.getErrors()[0].message)
+
+        verify(exactly = 0) { userGateway.create(any()) }
+    }
+
+    @Test
+    fun givenAInvalidParamsNullTypeUser_whenCallsCreateUser_thenReturnDomainException() {
+        val expectName = "Test Test"
+        val expectEmail = "teste@test.com"
+        val expectPassword = "aksnkdn@aksdl"
+        val expectTypeUser = null
+        val expectActivate = true
+        val exceptErrorMessage = "'typeUser' should not be null"
+
+        val aCommand = CreateUserCommand.with(expectName, expectEmail, expectPassword, expectTypeUser, expectActivate)
+
+        val actualException = Assertions.assertThrows(DomainException::class.java) { useCase.execute(aCommand) }
+        Assertions.assertEquals(exceptErrorMessage, actualException.getErrors()[0].message)
+
+        verify(exactly = 0) { userGateway.create(any()) }
+    }
+
+    @Test
+    fun givenAInvalidParamsNameLess3Character_whenCallGetawayThrows_thenReturnAException() {
+        val expectName = "Test Test"
+        val expectEmail = "test@test.com"
+        val expectPassword = "testdaaq12@"
+        val expectTypeUser = "ADMIN"
+        val expectActivate = true
+        val exceptErrorMessage = "Gateway error"
+
+        val aCommand = CreateUserCommand.with(expectName, expectEmail, expectPassword, expectTypeUser, expectActivate)
+
+        every { userGateway.create(any()) } throws IllegalStateException("Gateway error")
+
+        val actualException = Assertions.assertThrows(IllegalStateException::class.java) { useCase.execute(aCommand) }
+        Assertions.assertEquals(exceptErrorMessage, actualException.message)
+
+        verify(exactly = 1) { userGateway.create(any()) }
     }
 
 }
